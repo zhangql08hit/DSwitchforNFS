0da78afac8fb24a646a589583969cb665bf9224c
root <root@162105146166.net.pku.edu.cn>
2015-06-14 12:51:44 -0400

    fix a bug in checknfsops.sh

--- checknfsops.sh
+++ checknfsops.sh
@@ -5,7 +5,7 @@ a=`cat /proc/sys/fs/nfs/nlm_timeout`
 while [ $a -ne '0' ];
 do
 	sleep 0.5
-	a=`cat /proc/sys/fs/nfs/nlm_timeout`
+	a=`cat /proc/sys/fs/nfs/nfsd_zql_control`
 	echo "check again, $a"
 done
 echo 'done'
